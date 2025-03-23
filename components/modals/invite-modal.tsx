0081b5853c404d838b0cd0a9076069928849c8c1
tanaydas069 <tane.das135@gmail.com>
2025-03-23 11:13:55 +0530

    Enhance InviteModal with copy and regenerate invite link functionality

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -1,21 +1,60 @@
 "use client";
 
-import {Dialog,DialogContent,DialogHeader,DialogTitle} from "@/components/ui/dialog"
+import { Check, Copy, RefreshCw } from "lucide-react";
 
-import { useModal } from "@/hooks/use-modal-store";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle    
+} from "@/components/ui/dialog"
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Copy,RefreshCw } from "lucide-react";
+
+import { useModal } from "@/hooks/use-modal-store";
+import { useOrigin } from "@/hooks/use-origin";
+import { useState } from "react";
+import axios from "axios";
 
 
 
 
 export const InviteModal = () => {
-    const { isOpen, onClose, type } = useModal();
-   
-    const isModalOpen = isOpen && type === "invite";
+    const { onOpen, isOpen, onClose, type, data} = useModal();
+    const origin = useOrigin();
 
+    const isModalOpen = isOpen && type === "invite";
+    const { server } = data || {};
+
+    const [copied, setCopied] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const inviteUrl = `${origin}/invite/${server?.inviteCode}`;
+
+    const onCopy = () => {
+        navigator.clipboard.writeText(inviteUrl);
+        setCopied(true);
+
+        setTimeout(() => {
+            setCopied(false);
+        }, 1000);
+    }
+
+    const onNew = async () => {
+        try {
+            setIsLoading(true);
+            const response = await axios.patch(`/api/servers/${server?.id}/invite-code`);
+            
+            onOpen("invite", { server: response.data })
+
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
 
     return (
         <Dialog open={isModalOpen} onOpenChange={onClose}>
@@ -24,28 +63,37 @@ export const InviteModal = () => {
                     <DialogTitle className="text-2xl text-center font-bold">
                         Invite Friends
                     </DialogTitle>
-                    
+                    <DialogDescription className="text-center text-zinc-500">
+                    </DialogDescription>
                 </DialogHeader>
                 <div className="p-6">
-                    <Label className="uppercase-text-xs font-bold text-zinc-500 dark:text-secondary/70">
-                        Server Invite Link
+                    <Label
+                        className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70"
+                    >
+                        Server invite link
                     </Label>
                     <div className="flex items-center mt-2 gap-x-2">
-                        <Input  
-                            className="bg-zinc-300/50 border-0 focus:visible:ring-0 text-black focus-visible:ring-offset-0"
-                            value="invite-link"
+                        <Input
+                            disabled={isLoading}
+                            className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                            value={inviteUrl}
                         />
-                        <Button size="icon">
-                            <Copy className="w-4 h-4" />
+                        <Button disabled={isLoading} onClick={onCopy} size="icon">
+                            {copied 
+                                ? <Check className="w-4 h-4"/> 
+                                : <Copy className="w-4 h-4"/>
+                            }
                         </Button>
                     </div>
-                    <Button 
+                    <Button
+                        onClick={onNew}
+                        disabled={isLoading}
                         variant="link"
                         size="sm"
                         className="text-xs text-zinc-500 mt-4"
                     >
-                        Generate A New Link
-                        <RefreshCw className="w-4 h-4 ml-2" />
+                        Generate a new link
+                        <RefreshCw className="w-4 h-4 ml-2"/>
                     </Button>
                 </div>
             </DialogContent>

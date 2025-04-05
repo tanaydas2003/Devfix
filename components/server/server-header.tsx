6c59f0542a5a95c09162c7378056dcefaeda876e
tanaydas069 <tane.das135@gmail.com>
2025-04-05 19:40:47 +0530

    implemented manage member functionality

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -53,7 +53,8 @@ export const ServerHeader = ({
                     </DropdownMenuItem>
                 )}
                 {isAdmin && (
-                    <DropdownMenuItem   
+                    <DropdownMenuItem 
+                        onClick={() => onOpen("members",{server})}  
                         className="px-3 py-2 text-sm cursor-pointer"
                     >
                         Manage Members
